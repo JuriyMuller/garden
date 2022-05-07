65f702b1ae788a3e762a7b44331513bdd8229c47
Nastya Semyonova <nastya.semyonova.ss@gmail.com>
2022-05-07 23:01:46 +0600

    second commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ let options = {
     offset: 500
   }
 let header = new Headhesive('.header',options);
+
+let link = document.querySelector(".navbar-link");
+link.addEventListener("click",()=>{
+  link.classList.toggle("active");
+
+  if (link.classList.contains("active")) {
+    link.style.display = "block";
+  } else {
+    link.style.display = "none";
+  }
+})
+header.destroy();
